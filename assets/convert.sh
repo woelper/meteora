e9881a98cd536afd91064833e9296f514d38269d
Johann Woelper <woelper@gmail.com>
2023-10-16 20:03:02 +0200

    add icon conversion script

---
+++ assets/convert.sh
@@ -0,0 +1,5 @@
+# convert -size 1024x1024 logo.svg test.png
+convert -resize 256x256 icon-1024.png icon-256.png
+convert -resize 192x192 icon-1024.png icon_ios_touch_192.png
+convert -resize 512x512 icon-1024.png maskable_icon_x512.png
+optipng -o4 -clobber *.png
